a7dbf6eee44f99618d280690f8154938fd577cf3
Dean Leonard <deanleonard26@gmail.com>
2020-11-27 18:51:01 +0000

    Dean

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function fetchSimpson() {
+    const url = 'https://simpsons-quotes-api.herokuapp.com/quotes'
+    axios.get(url)
+      .then(function(simpson) {
+        return simpson.data; 
+      })
+      .then(function(simpson) {
+        console.log('data decoded from JSON:', simpson);
+
+        const simpsonHtml = `
+        <h3><strong>${simpson[0].character}</strong></h3>
+          <p><strong>${simpson[0].quote}</strong></p>
+          <img src="${simpson[0].image}" />
+        `;
+        document.querySelector('#simpson').innerHTML = simpsonHtml;
+      });
+  }
+  
+  fetchSimpson();
